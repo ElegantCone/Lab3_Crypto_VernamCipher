4dea1c7a70cdc8a49d3cf3bf96d5e14fbb5f8203
Екатерина Сараева <e.saraeva@g.nsu.ru>
2021-10-12 13:32:32 +0700

    Added coder and encoder

--- Lab3_Crypto_Vernam_Cipher/Program.cs
+++ Lab3_Crypto_Vernam_Cipher/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace Lab3_Crypto_Vernam_Cipher
+﻿using System;
+
+namespace Lab3_Crypto_Vernam_Cipher
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            ExampleEng();
+            ExampleRus();
         }
+
+
+        static void ExampleEng()
+        {
+            Console.WriteLine("---English encoder example---");
+            string text = "hello";
+            var alg = new VernamCoder(text);
+            string encoded = alg.Encode();
+            string decoded = new VernamCoder(encoded).Encode();
+            Console.WriteLine("Text: {0}", text);
+            Console.WriteLine("Encrypt: {0}", encoded);
+            Console.WriteLine("Decrypt: {0}", decoded);
+        }
+
+        static void ExampleRus()
+        
+        {
+            Console.WriteLine("---Пример русского кодировщика---");
+            string text = "вышел грека через реку видит грека в реке рак";
+            var alg = new VernamCoder(text);
+            string encoded = alg.Encode();
+            string decoded = new VernamCoder(encoded).Encode();
+            Console.WriteLine("Текст: {0}", text);
+            Console.WriteLine("Зашифрованный текст: {0}", encoded);
+            Console.WriteLine("Расшифрованный текст: {0}", decoded);
+        }
+        
+        
     }
 }
